1e012074abf7409a6d3bba6f108ed0f2c49b3d9e
YogeshR011 <76857953+YogeshR011@users.noreply.github.com>
2025-04-23 15:36:25 +0530

    small fix

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push yogeshkumar011/simple-python-flask-app 
+docker pull yogeshkumar011/simple-python-flask-app 
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 yogeshkumar011/simple-python-flask-app 
