4e8b9db4236fe0e70c81592bdec7dc4b920c8baf
YogeshR011 <76857953+YogeshR011@users.noreply.github.com>
2025-04-23 15:30:12 +0530

    added files for code deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker push yogeshkumar011/simple-python-flask-app 
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 yogeshkumar011/simple-python-flask-app 
